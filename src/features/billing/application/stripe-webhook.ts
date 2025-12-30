c5b8cd65c34f7d51400d1ef486d0e45c0f5c4224
ResidencyWorks <residencyworks@gmail.com>
2025-12-30 17:44:47 -0500

    Update stripe-webhook.ts

--- src/features/billing/application/stripe-webhook.ts
+++ src/features/billing/application/stripe-webhook.ts
@@ -36,7 +36,9 @@ export async function handleStripeWebhookRequest(
 		}
 
 		const stripe = new Stripe(env.STRIPE_SECRET_KEY ?? "", {
-			apiVersion: "2025-11-17.clover",
+  apiVersion: "2025-12-15.clover",
+});
+,
 		});
 
 		const webhookSecret = env.STRIPE_WEBHOOK_SECRET;
